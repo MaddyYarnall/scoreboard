6195453d8abab289d7a090c120a9456ba682626e
Madison Yarnall <maddycodes01@gmail.com>
2021-12-06 15:16:25 -0700

    changes

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-function points() {
-  console.log("Point added")
+
+let teamOnePoints = 0
+let teamTwoPoints = 0
+
+
+function teamOne() {
+  teamOnePoints++
+  document.getElementById("teamonepoints").innerText = teamOnePoints.toString()
 }
+
+function teamTwo(){
+  teamTwoPoints++
+  document.getElementById("teamtwopoints").innerText = teamTwoPoints.toString()
+
+}
+
